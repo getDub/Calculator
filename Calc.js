29461e982b6993c7ae32f5533bf9aad1e3ce0ff5
getDub <143089345+getDub@users.noreply.github.com>
2023-11-28 14:21:02 +1100

    Arithmetic functions created with reduce

---
+++ Calc.js
@@ -0,0 +1,33 @@
+let numbers = [ 1, 2, 3, 4, 5 ];
+let numbers2 = [ 5, 2 ];
+let numbers3 = [ 15, 32 ];
+let numbers4 = [ 5, 4, 3, 2, 1 ];
+
+
+//Add function
+const add = function ( arr ) {
+    return arr.reduce((total, number) => total + number,0);
+}
+
+console.log( add( numbers4 ) );
+
+//Subtract function
+const subtract = function ( arr ) {
+    return arr.reduce((total, currentNo) => total - currentNo);
+}
+
+console.log( subtract( numbers4 ) );
+
+//Multiply function 
+const multiply = function ( arr ) {
+    return arr.reduce((total, currentNo) => total * currentNo, 1);
+}
+
+console.log( multiply( numbers4 ) );
+
+//Divide function 
+const divide = function( arr ) {
+    return arr.reduce((total, currentNo) => total / currentNo);
+}
+
+console.log( divide( numbers4 ) );

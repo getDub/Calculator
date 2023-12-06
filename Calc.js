1e1e9865acf6d31eade6bb4978dcc71103f96051
getDub <143089345+getDub@users.noreply.github.com>
2023-12-06 13:23:18 +1100

    All arithmatic functions changed to reduce methods

--- Calc.js
+++ Calc.js
@@ -7,25 +7,24 @@ const clear = document.querySelector('.clear');
 const equals = document.querySelector('.equals');
 
 
+
 //Parts of the calculator operation
 let displayValues = '';
 let a;
-let operator = '';
+let op = '';
 let b;
+let finalTotal;
 
 
 
 //Add event listener to all the buttons with a class of nums.
 nums.forEach((button) => {
-    
+     
     button.addEventListener('click', () => {
         
-        
-        let input = display.textContent = button.textContent //when number button is pressed it shows that number in the '.display' div.
-        displayValues += input; //Adds number presses to the displayValues string.
-        display.textContent = displayValues;
-        
-                
+        displayValues += button.textContent; //Adds number presses to the displayValues string.
+        a = display.textContent = displayValues; //when number button is pressed it shows that number in the '.display' div.
+        // display.textContent = input;
     });
 })
 
@@ -33,36 +32,45 @@ nums.forEach((button) => {
 
 
 //Event listners for operator buttons
-operators.forEach((operation) => {
-    operation.addEventListener('click', () => {
+let operations = function () {
+    
+    operators.forEach((operation) => {
         
-        let subTotal = operate( displayValues ); //Call function to reduce a and b to its sub total.
-        display.textContent = subTotal; // Display sub total.
-        operator  = operation.textContent; // Registers operator from elements text Content.
-    //    console.log(operator)
-        displayValues += operator; // Pushes operator to displayValues variable.
-        // console.log(displayValues)
-        
+        operation.addEventListener('click', () => {
+            
+            display.textContent = a;
+            console.log(a)
+            let subTotal = operate( displayValues ); //Call function to reduce a and b to its sub total.
+            display.textContent =  subTotal; // Display sub total.
+            op  = operation.textContent; // Registers operator from elements text Content.
+            displayValues += op; // Pushes operator to displayValues variable.
+            
+        })
         
     })
-    
-})
+}
+operations();
 
 
+
+const equalsBtn = function (){
 equals.addEventListener('click', () => {
     
-    let finalTotal = operate( displayValues );
+    finalTotal = operate( displayValues );
     console.log(finalTotal);
+    
     displayValues += finalTotal;
-    return display.textContent = finalTotal;
+    display.textContent = finalTotal;
+    
 })
-
+}
+equalsBtn();
 
 
 clear.addEventListener('click', () => {
     display.textContent = '';
     firstNumber = '';
-    operator = '';
+    op = '';
     secondNumber = '';
 })
 
@@ -70,25 +78,27 @@ clear.addEventListener('click', () => {
 
 
 //Add function
-const add = function ( [a,b] ) {
-    
+const add = function ( [a,b] ) 
+{
     return displayValues = [a,b].reduce((total, current) => total + current, 0)
-    
 }
 
 //Subtract function
-const subtract = function ( [a, b] ) {
-    return displayValues = [a - b].reduce((total, current) => total - current, 0);
+const subtract = function ( [a,b] ) 
+{
+    return displayValues = [a,b].reduce((total, current) => total - current )
 }
 
 //Multiply function 
-const multiply = function ( a, b ) {
-    return a * b;
+const multiply = function ( [a, b] ) 
+{
+    return displayValues = [a,b].reduce((total, current) => current * total, 1 )
 }
 
 //Divide function 
-const divide = function( a, b ) {
-    return a / b;
+const divide = function( [a, b] ) 
+{
+    return displayValues = [a,b].reduce((total, current) => total / current)
 }
 
 
@@ -101,14 +111,18 @@ const operate = function ( equation ) {
     a = +str[0];
     op = str[1];
     b = +str[2];
-
-if (op === "+") return add ([a,b]);
-if (op === "-") return subtract([a,b]); 
-if (op === "*") return multiply([a,b]); 
-if (op === "/") return divide([a,b]); 
-
-
+    console.log(`a ${a}`)
+    console.log(`op ${op}`)
+    console.log(`b ${b}`)
+    
+    if (op === "+") return add ([a,b]);
+    if (op === "-") return subtract([a,b]); 
+    if (op === "*") return multiply([a,b]); 
+    if (op === "/") return divide([a,b]); 
+    
+    
 }
+
 
 
 
